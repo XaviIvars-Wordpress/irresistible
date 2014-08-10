5d3ca8f387630d7c60fc0c569aea92691d525136
Xavi Ivars <xavi.ivars@gmail.com>
2014-08-10 17:15:46 +0200

    Moving script loading to the bottom of the page

--- includes/theme-js.php
+++ includes/theme-js.php
@@ -1,8 +1,7 @@
 <?php
 if (!is_admin()) add_action( 'wp_print_scripts', 'woothemes_add_javascript' );
 function woothemes_add_javascript( ) {
-	wp_enqueue_script('jquery');    
-	wp_enqueue_script( 'tabs', get_bloginfo('template_directory').'/includes/js/tabs.js', array( 'jquery' ) );
-	wp_enqueue_script( 'superfish', get_bloginfo('template_directory').'/includes/js/superfish.js', array( 'jquery' ) );
+	wp_enqueue_script( 'tabs', get_bloginfo('template_directory').'/includes/js/tabs.js', array( 'jquery' ) , false, true);
+	wp_enqueue_script( 'superfish', get_bloginfo('template_directory').'/includes/js/superfish.js', array( 'jquery' ), false, true );
 }
 ?>
